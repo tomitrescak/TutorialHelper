3b6afa35bc2146163df05c9e3d00a82e4450dc53
Tomas Trescak <tomi.trescak@gmail.com>
2016-08-08 17:34:03 +1000

    Going to modify marking ...

--- src/server/configs/accounts.ts
+++ src/server/configs/accounts.ts
@@ -1,6 +1,10 @@
 import { Accounts } from 'meteor/accounts-base';
 
+Accounts.emailTemplates['resetPassword'].subject = () => '[SCEM] Computer Security Lab - Verification';
+Accounts.emailTemplates['verifyEmail'].subject = () => '[SCEM] Computer Security Lab - Reset Password';
+
 export default function() {
+
   Accounts.config({
     sendVerificationEmail: false
   });
--- src/server/data/fixtures.ts
+++ src/server/data/fixtures.ts
@@ -5,22 +5,22 @@ import { Exercises, Possibilities, Questions } from './schemas/exercise_schema';
 
 
 export default function() {
-  if (!Semesters.findOne()) {
-    let pos = { question: 'q1', answer: 'a1'};
-    let posid = Possibilities.insert({ possibilities: [pos]});
-    let q1 = Questions.insert({
-      description: 'Question 1',
-      possibilitiesGroupId: posid
-    });
-    let q2 = Questions.insert({
-      description: 'Question 2',
-      question: 'q2',
-      expectedAnswer: 'a2',
-      control: 'textbox'
-    });
+  // if (!Semesters.findOne()) {
+  //   let pos = { question: 'q1', answer: 'a1'};
+  //   let posid = Possibilities.insert({ possibilities: [pos]});
+  //   let q1 = Questions.insert({
+  //     description: 'Question 1',
+  //     possibilitiesGroupId: posid
+  //   });
+  //   let q2 = Questions.insert({
+  //     description: 'Question 2',
+  //     question: 'q2',
+  //     expectedAnswer: 'a2',
+  //     control: 'textbox'
+  //   });
     
-    let ex1id = Exercises.insert({name: 'Exercise1', instructions: 'instructions', questions: [q1, q2], points: 1 })
-    const pid =  Practicals.insert({name: 'Week 3', description: 'Week 3 exercises', exercises: [ex1id]})
-    Semesters.insert({  name: 'Spring 2016', practicals: [pid] })
-  }
+  //   let ex1id = Exercises.insert({name: 'Exercise1', instructions: 'instructions', questions: [q1, q2], points: 1 })
+  //   const pid =  Practicals.insert({name: 'Week 3', description: 'Week 3 exercises', exercises: [ex1id]})
+  //   Semesters.insert({  name: 'Spring 2016', practicals: [pid] })
+  // }
 }
